17050e5127350045a8e7d8eef4e38a7a0a3f8b14
Pontus Gustafsson <gustafsson.pontus@live.se>
2021-07-10 19:44:19 +0200

    Added gameobject mask in Constants.h

--- Constants.h
+++ Constants.h
@@ -8,4 +8,5 @@ const int PLAYER_SIZE = 128;
 const float SCALE_FACTOR_X = PLAYER_SIZE / DEFAULT_WINDOW_WIDTH;
 const float SCALE_FACTOR_Y = PLAYER_SIZE / DEFAULT_WINDOW_HEIGHT;
 const float GRAVITY = 9.82;
+const int TERRAIN_MASK = 0;
 #endif
